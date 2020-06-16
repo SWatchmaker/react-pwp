2e9426b7567ddd7678766f4a7ec59eef14e53f09
Sebastián Weidmann <sebastian.weidmann.l@gmail.com>
2020-06-16 09:33:36 -0400

    avances loader

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Loader from "./Loader";
 
-const Footer = (props) => {
+const Footer = props => {
   const [loadInit, setLoadInit] = useState(false);
   const [show, setShow] = useState(false);
 
@@ -43,17 +43,17 @@ const Footer = (props) => {
                 <a
                   href="https://www.linkedin.com/in/sebaweidmann/"
                   target="_blank"
-                  className={`animate__animated ${
+                  className={`animate__animated animate__delay-1s ${
                     show ? "animate__fadeInLeft" : "animate__fadeOutRight"
                   }`}
                   style={{
-                    "--animate-delay": "0.4s",
+                    "--animate-delay": "0.4s"
                   }}
                 >
                   <i
                     className="fab fa-linkedin-in"
                     style={{
-                      color: "#0077b5",
+                      color: "#0077b5"
                     }}
                   ></i>
                   <span>Conoce mi CV</span>
@@ -63,17 +63,17 @@ const Footer = (props) => {
                 <a
                   href="https://github.com/SWatchmaker"
                   target="_blank"
-                  className={`animate__animated ${
+                  className={`animate__animated animate__delay-1s ${
                     show ? "animate__fadeInLeft" : "animate__fadeOutRight"
                   }`}
                   style={{
-                    "--animate-delay": "0.8s",
+                    "--animate-delay": "0.8s"
                   }}
                 >
                   <i
                     className="fab fa-github"
                     style={{
-                      color: "#6e5494",
+                      color: "#6e5494"
                     }}
                   ></i>
                   <span>Mis experimentos</span>
@@ -83,17 +83,17 @@ const Footer = (props) => {
                 <a
                   href="mailto:sebastian.weidmann.l@gmail.com"
                   target="_blank"
-                  className={`animate__animated ${
+                  className={`animate__animated animate__delay-1s ${
                     show ? "animate__fadeInLeft" : "animate__fadeOutRight"
                   }`}
                   style={{
-                    "--animate-delay": "1.2s",
+                    "--animate-delay": "1.2s"
                   }}
                 >
                   <i
                     className="far fa-envelope"
                     style={{
-                      color: "#ea4335",
+                      color: "#ea4335"
                     }}
                   ></i>
                   <span>Escríbeme!</span>
@@ -103,17 +103,17 @@ const Footer = (props) => {
                 <a
                   href="https://wa.link/io5hce"
                   target="_blank"
-                  className={`animate__animated ${
+                  className={`animate__animated animate__delay-1s ${
                     show ? "animate__fadeInLeft" : "animate__fadeOutRight"
                   }`}
                   style={{
-                    "--animate-delay": "1.4s",
+                    "--animate-delay": "1.4s"
                   }}
                 >
                   <i
                     className="fab fa-whatsapp"
                     style={{
-                      color: "#25d366",
+                      color: "#25d366"
                     }}
                   ></i>
                   <span>Whatsapéame!</span>
@@ -132,17 +132,17 @@ const Footer = (props) => {
                 <a
                   href="https://www.instagram.com/xoclomaster/"
                   target="_blank"
-                  className={`animate__animated ${
+                  className={`animate__animated animate__delay-1s ${
                     show ? "animate__fadeInRight" : "animate__fadeOutLeft"
                   }`}
                   style={{
-                    "--animate-delay": "0.4s",
+                    "--animate-delay": "0.4s"
                   }}
                 >
                   <i
                     className="fab fa-instagram-square"
                     style={{
-                      color: "#c13584",
+                      color: "#c13584"
                     }}
                   ></i>
                   <span>Instagram</span>
@@ -152,17 +152,17 @@ const Footer = (props) => {
                 <a
                   href="https://www.facebook.com/SW.Watchmaker/"
                   target="_blank"
-                  className={`animate__animated ${
+                  className={`animate__animated animate__delay-1s ${
                     show ? "animate__fadeInRight" : "animate__fadeOutLeft"
                   }`}
                   style={{
-                    "--animate-delay": "0.4s",
+                    "--animate-delay": "0.8s"
                   }}
                 >
                   <i
                     className="fab fa-facebook"
                     style={{
-                      color: "#3b5998",
+                      color: "#3b5998"
                     }}
                   ></i>
                   <span>Facebook</span>
@@ -172,17 +172,17 @@ const Footer = (props) => {
                 <a
                   href="https://twitter.com/sawlrock"
                   target="_blank"
-                  className={`animate__animated ${
+                  className={`animate__animated animate__delay-1s ${
                     show ? "animate__fadeInRight" : "animate__fadeOutLeft"
                   }`}
                   style={{
-                    "--animate-delay": "0.4s",
+                    "--animate-delay": "1.2s"
                   }}
                 >
                   <i
                     className="fab fa-twitter"
                     style={{
-                      color: "#1da1f2",
+                      color: "#1da1f2"
                     }}
                   ></i>
                   <span>Twitter</span>
@@ -193,17 +193,17 @@ const Footer = (props) => {
                 <a
                   href="https://steamcommunity.com/id/SWatchmaker/"
                   target="_blank"
-                  className={`animate__animated ${
+                  className={`animate__animated animate__delay-1s ${
                     show ? "animate__fadeInRight" : "animate__fadeOutLeft"
                   }`}
                   style={{
-                    "--animate-delay": "0.4s",
+                    "--animate-delay": "1.6s"
                   }}
                 >
                   <i
                     className="fab fa-steam"
                     style={{
-                      color: "#00adee",
+                      color: "#00adee"
                       //#C28F2C
                     }}
                   ></i>
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,55 +1,191 @@
 import React, { useEffect, useState } from "react";
 import Loader from "../components/Loader";
-import { useLayoutEffect } from "react";
 
 const WelcomPage = () => {
+  const rotDif = 220 / 5;
+  const initialState = {
+    top: 50,
+    left: 50,
+    speed: 1
+  };
+  const [circleAnim, setCircleAnim] = useState(true);
   const [nodePosition, setNodePos] = useState({
-    top: Math.random() * 100,
-    left: Math.random() * 100,
+    ...initialState,
+    rotation: 250 + rotDif
+  });
+  const [htmlPosition, setHtmlPos] = useState({
+    ...initialState,
+    rotation: 250 + rotDif * 2
+  });
+  const [reactPosition, setReactPos] = useState({
+    ...initialState,
+    rotation: 250 + rotDif * 3
+  });
+  const [cssPosition, setCssPos] = useState({
+    ...initialState,
+    rotation: 250 + rotDif * 4
   });
 
   useEffect(() => {}, []);
 
-  function getViewport() {
-    var viewPortWidth;
-    var viewPortHeight;
-
-    // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
-    if (typeof window.innerWidth != "undefined") {
-      (viewPortWidth = window.innerWidth),
-        (viewPortHeight = window.innerHeight);
-    }
-
-    // IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
-    else if (
-      typeof document.documentElement != "undefined" &&
-      typeof document.documentElement.clientWidth != "undefined" &&
-      document.documentElement.clientWidth != 0
-    ) {
-      (viewPortWidth = document.documentElement.clientWidth),
-        (viewPortHeight = document.documentElement.clientHeight);
-    }
-
-    // older versions of IE
-    else {
-      (viewPortWidth = document.getElementsByTagName("body")[0].clientWidth),
-        (viewPortHeight = document.getElementsByTagName("body")[0]
-          .clientHeight);
-    }
-    return [viewPortWidth, viewPortHeight];
+  function startBounce() {
+    setCircleAnim(false);
+    setNodePos(createPos(nodePosition));
+    setHtmlPos(createPos(htmlPosition));
+    setReactPos(createPos(reactPosition));
+    setCssPos(createPos(cssPosition));
   }
+
+  function createPos(oldpos) {
+    let pos = {};
+    let moves = [
+      { top: 0, left: Math.random() * 100 },
+      { top: 100, left: Math.random() * 100 },
+      { left: 0, top: Math.random() * 100 },
+      { left: 100, top: Math.random() * 100 }
+    ];
+
+    if (
+      oldpos.top != 0 &&
+      oldpos.left != 0 &&
+      oldpos.top != 100 &&
+      oldpos.left != 100
+    ) {
+      const ran = Math.floor(Math.random() * 4);
+      pos = moves[ran];
+    } else {
+      const ran = Math.floor(Math.random() * 3);
+      if (oldpos.top == 0) {
+        moves.shift();
+      } else if (oldpos.top == 100) {
+        moves.splice(1, 1);
+      } else if (oldpos.left == 0) {
+        moves.splice(2, 1);
+      } else if (oldpos.left == 100) {
+        moves.pop();
+      }
+      console.log(moves);
+      pos = moves[ran];
+    }
+
+    const container = document.getElementById("bounceContainer");
+    const contHeight = container.offsetHeight;
+    const contWidth = container.offsetWidth;
+    const movement = Math.sqrt(
+      (Math.abs(oldpos.left - pos.left) * (contWidth / 100)) ** 2 +
+        (Math.abs(oldpos.top - pos.top) * (contHeight / 100)) ** 2
+    );
+    const speed = movement / 300;
+    pos.speed = speed;
+    return pos;
+  }
+
   return (
     <div className="welcomePage">
       <Loader />
       <button className="welcomePage__inButton">BIENVENIDO</button>
-      <div
-        className="welcomePage__rollIcon"
-        style={{ top: `${nodePosition.top}%`, left: `${nodePosition.left}%` }}
-      >
-        <i class="fab fa-html5"></i>
-      </div>
-      <div className="welcomePage__rollIcon">
-        <i class="fab fa-node-js"></i>
+      <div className="welcomePage__bounceContainer" id="bounceContainer">
+        <div
+          className={`animate__animated animate__fadeIn animate__delay-1s ${
+            circleAnim
+              ? "welcomePage__rollIcon--circle"
+              : "welcomePage__rollIcon"
+          }`}
+          style={{
+            top: `${nodePosition.top}%`,
+            left: `${nodePosition.left}%`,
+            transition: `all ${nodePosition.speed}s linear`,
+            "--animate-delay": `0s`,
+            transform: circleAnim
+              ? `rotate(${nodePosition.rotation}deg)`
+              : "none"
+          }}
+          onTransitionEnd={e => {
+            if (e.propertyName == "top" && !circleAnim) {
+              const oldpos = nodePosition;
+              setNodePos(createPos(oldpos));
+            }
+          }}
+        >
+          <i className="fab fa-node-js fa-3x" style={{ color: "#68A063" }}></i>
+        </div>
+        <div
+          className={`animate__animated animate__fadeIn animate__delay-1s ${
+            circleAnim
+              ? "welcomePage__rollIcon--circle"
+              : "welcomePage__rollIcon"
+          }`}
+          style={{
+            top: `${htmlPosition.top}%`,
+            left: `${htmlPosition.left}%`,
+            transition: `all ${htmlPosition.speed}s linear`,
+            "--animate-delay": `0.6s`,
+            transform: circleAnim
+              ? `rotate(${htmlPosition.rotation}deg)`
+              : "none"
+          }}
+          onTransitionEnd={e => {
+            if (e.propertyName == "top" && !circleAnim) {
+              const oldpos = htmlPosition;
+              setHtmlPos(createPos(oldpos));
+            }
+          }}
+        >
+          <i className="fab fa-html5 fa-3x" style={{ color: "#E44D26" }}></i>
+        </div>
+        <div
+          className={`animate__animated animate__fadeIn animate__delay-1s ${
+            circleAnim
+              ? "welcomePage__rollIcon--circle"
+              : "welcomePage__rollIcon"
+          }`}
+          style={{
+            top: `${reactPosition.top}%`,
+            left: `${reactPosition.left}%`,
+            transition: `all ${reactPosition.speed}s linear`,
+            "--animate-delay": `1.2s`,
+            transform: circleAnim
+              ? `rotate(${reactPosition.rotation}deg)`
+              : "none"
+          }}
+          onTransitionEnd={e => {
+            if (e.propertyName == "top" && !circleAnim) {
+              const oldpos = reactPosition;
+              setReactPos(createPos(oldpos));
+            }
+          }}
+        >
+          <i className="fab fa-react fa-3x" style={{ color: "#61dafb" }}></i>
+        </div>
+        <div
+          className={`animate__animated animate__fadeIn animate__delay-1s ${
+            circleAnim
+              ? "welcomePage__rollIcon--circle"
+              : "welcomePage__rollIcon"
+          }`}
+          style={{
+            top: `${cssPosition.top}%`,
+            left: `${cssPosition.left}%`,
+            transition: `all ${cssPosition.speed}s linear`,
+            "--animate-delay": `1.8s`,
+            transform: circleAnim
+              ? `rotate(${cssPosition.rotation}deg)`
+              : "none"
+          }}
+          onTransitionEnd={e => {
+            if (e.propertyName == "top" && !circleAnim) {
+              const oldpos = cssPosition;
+              setCssPos(createPos(oldpos));
+            }
+          }}
+          onAnimationEnd={() => {
+            setTimeout(() => {
+              startBounce();
+            }, 600);
+          }}
+        >
+          <i className="fab fa-css3-alt fa-3x" style={{ color: "#2965f1" }}></i>
+        </div>
       </div>
     </div>
   );
